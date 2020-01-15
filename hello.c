dd8c73fc90f6a265ff8bddac6e36f77827ecc9dd
Wangpf <1655446537@qq.com>
2020-01-15 11:34:09 +0800

    remove nouse

--- hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("hello world\n");
-	printf("this is a git learn\n");
-}
--- hello.py
+++
@@ -1,4 +0,0 @@
-print('hello world')
-print('this is a git learn')
-
-print('git has a mutable index called stage')
